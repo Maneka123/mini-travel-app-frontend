a124dda293bb8825e4f9cdc2a2e7b29cba00dc19
Maneka Wijesundara <maneka611997@gmail.com>
2026-03-10 19:48:48 +0530

    create my listing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Listings from "./pages/Listings";
-import CreateListing from "./pages/CreateMyList";
+import CreateMyList from "./pages/CreateMyList";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
           <Route path="/" element={<Listings />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/create" element={<CreateListing />} /> {/* new route */}
+          <Route path="/create" element={<CreateMyList />} /> {/* new route */}
         </Routes>
       </main>
     </Router>
